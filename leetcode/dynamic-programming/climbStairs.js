26f44a29e16c32e077b789c2c0b9c3c877797a48
chengkangjian <chengkangjian@megvii.com>
2019-06-12 11:36:13 +0800

    :art: dynamic-programming

--- leetcode/dynamic-programming/climbStairs.js
+++ leetcode/dynamic-programming/climbStairs.js
@@ -23,7 +23,7 @@
  * @param {number} n
  * @return {number}
  */
-// 错误解法：
+// 异常解法：当数值较大时，运行超时，毕竟递归的方式性能消耗较大
 // var climbStairs = function(n) {
 //   if(n === 1) {
 //     return 1
@@ -35,6 +35,7 @@
 //     return climbStairs(n-2) + climbStairs(n-1)
 //   }
 // };
+
 // 正确解法：
 var climbStairs = function(n) {
   const dp = []
@@ -49,8 +50,10 @@ var climbStairs = function(n) {
   for (let i = 2; i < n; i++) {
     dp[i] = dp[i - 2] + dp[i - 1]
   }
-  return dp.pop()
+  return dp[n-1]
 }
+
+
 
 // 这个问题可以被分解为一些包含 **最优子结构的子问题**，即它的最优解可以从其子问题的最优解来有效地构建，我们可以使用动态规划来解决这一问题。
 
@@ -64,4 +67,4 @@ var climbStairs = function(n) {
 // dp[i] = dp[i-1] + dp[i-2]
 
 // console.log(climbStairs(5))
-console.log(climbStairs(10)) //89
+console.log(climbStairs(50)) //89
