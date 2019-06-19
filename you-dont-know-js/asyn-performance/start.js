d52c3b70854cc2fe9f9d5a4656c30222b66767e3
chengkangjian <chengkangjian@megvii.com>
2019-06-19 17:47:04 +0800

    v1.1版本更新

---
+++ you-dont-know-js/asyn-performance/start.js
@@ -0,0 +1,10 @@
+function now() {
+  return 21;
+}
+function later() {
+  answer = answer * 2;
+  console.log("Meaning of life:", answer);
+}
+
+let answer = now();
+setTimeout(later, 1000); // Meaning of life: 42
