bc06a1c94e9014bf47022af668775df73a99bc01
chengkangjian <chengkangjian@megvii.com>
2020-07-21 16:55:28 +0800

    ...

---
+++ self-hetu-gl/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DefaultPlayound from "./DefaultPlayground";
+
+function App() {
+  return (
+    <div className="App">
+      <DefaultPlayound></DefaultPlayound>
+    </div>
+  );
+}
+
+export default App;
---
+++ self-hetu-gl/src/DefaultPlayground/index.jsx
@@ -0,0 +1,33 @@
+import { Engine, Scene } from "react-babylonjs";
+import React from "react";
+import { Vector3 } from "@babylonjs/core";
+import "./index.css";
+const DefaultPlayground = () => (
+  <Engine canvasId="sample-canvas">
+    <Scene>
+      <arcRotateCamera
+        name="camera11"
+        position={new Vector3(0, 5, -10)}
+        target={Vector3.Zero()}
+        alpha={-(Math.PI / 2)}
+        beta={Math.PI / 3}
+        radius={10}
+      />
+      <hemisphericLight
+        name="light11"
+        intensity={0.7}
+        direction={Vector3.Up()}
+      />
+      <sphere
+        name="sphere11"
+        diameter={2}
+        segments={16}
+        position={new Vector3(0, 1, 0)}
+      />
+
+      <ground name="ground1" width={6} height={6} subdivisions={2} />
+    </Scene>
+  </Engine>
+);
+
+export default DefaultPlayground;
